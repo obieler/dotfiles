069a37332189744578eb714a17f45a07396cd603
Olivier Bieler <olivier.bieler@epfl.ch>
2020-02-03 16:51:56 +0100

    Add Atom packages installer

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# dotfiles
+dotfiles
+========
+
+Config files
+------------
+* Git -> [.gitconfig](.gitconfig)
+* Shutter -> [.shutter](.shutter/)
+
+Install Atom packages
+---------------------
+`./bin/install-atom-packages.sh`
---
+++ bin/install-atom-packages.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+
+apm install atom-beautify autocomplete-python busy-signal django-templates editorconfig \
+intentions language-docker linter linter-eslint linter-ui-default markdown-pdf multi-cursor \
+pigments seti-icons teletype activate-power-mode
